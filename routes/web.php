5848290530f75d3cc0c9f643014188c585a67bf5
Muhammad Raihan <raihm87@gmail.com>
2024-09-08 17:33:28 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Http\Controllers\tampilanLimaController;
-use App\Http\Controllers\tampilanEmpatController;
-use App\Http\Controllers\tampilanTigaController;
-use App\Http\Controllers\tampilanDuaController;
-use App\Http\Controllers\tampilanSatuController;
 use App\Http\Controllers\tampilanController;
 use App\Http\Controllers\loginController;
 use App\Http\Controllers\registerController;
@@ -14,11 +9,6 @@ Route::get('/', [loginController::class, 'index']);
 Route::get('/login', [loginController::class, 'login']);
 Route::get('/register', [registerController::class, 'register']);
 Route::get('/tampilan_awal', [tampilanController::class, 'tampilan_awal']);
-Route::get('/tampilan_satu', [tampilanSatuController::class, 'tampilan_satu']);
-Route::get('/tampilan_dua', [tampilanDuaController::class, 'tampilan_dua']);
-Route::get('/tampilan_tiga', [tampilanTigaController::class, 'tampilan_tiga']);
-Route::get('/tampilan_empat', [tampilanEmpatController::class, 'tampilan_empat']);
-Route::get('/tampilan_lima', [tampilanLimaController::class, 'tampilan_lima']);
 
 Route::get('/',function(){
     return view('home');
