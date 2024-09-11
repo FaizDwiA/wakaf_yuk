6ccb0b721d3be7553b62256d8a64f2ddf386edfe
Faiz <fdwi99465@gmail.com>
2024-09-11 10:09:55 +0700

    Ngehapus yang ngga perlu

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,6 @@ Route::get('/login', [loginController::class, 'login']);
 Route::get('/register', [registerController::class, 'register']);
 Route::get('/tampilan_awal', [tampilanController::class, 'tampilan_awal']);
 
-Route::get('/',function(){
-    return view('home');
-});
-
-Route::get('/about',function(){
-    return view('about', ['nama' => 'Faiz']);
-});
-
-Route::get('/blog',function(){
-    return view('blog');
-});
-
-Route::get('/contact',function(){
-    return view('contact');
-});
-
 Route::get('/tampilan',function(){
     return view('tampilan_awal');
 });
