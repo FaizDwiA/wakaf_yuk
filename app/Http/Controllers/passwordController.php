2075ea4f0f7c08668a640eda6aaee4d59b5ce8d3
Faiz <fdwi99465@gmail.com>
2024-10-14 09:49:55 +0700

    folder

---
+++ app/Http/Controllers/passwordController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+class passwordController extends Controller
+{
+    function password() 
+    {
+        return view('password');
+    }
+}
